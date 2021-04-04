e6710b05d5f5ae971eca24f8367016482b172a08
Nimasha-Perera <73678949+Nimasha-Perera@users.noreply.github.com>
2021-04-04 08:37:01 +0530

    Add files via upload

---
+++ Controllers/ParticipantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ParticipantController : ApiController
+    {
+        [HttpPost]
+    [Route("api/InsertParticipant") ]
+        public Participant Insert(Participant model)
+        {
+            using (DBmodel db = new DBmodel ()){
+                db.Participants.Add(model);
+                db.SaveChanges();
+                return model;
+
+            }
+        }
+        [HttpPost]
+        [Route("api/UpdateOutput")]
+        public void UpdateOutput(Participant model)
+        {
+            using (DBmodel db = new DBmodel())
+            {
+                db.Entry(model).State = System.Data.EntityState.Modified;
+                db.SaveChanges();
+         
+
+            }
+        }
+    }
+}
